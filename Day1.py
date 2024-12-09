152d8aa6549fa6d63439ef744a25df137560486d
Gentes720 <lontsiangelin@gmail.com>
2024-12-09 23:45:18 +0100

    adding of day 1

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print('Welcome to the Brand Name Generator.')
+print("Your band name could be " + 
+      input("What's the name of the city you grew up in ?\n") + " " 
+      + input("What's your pet's name ?\n"))
