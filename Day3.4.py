2c948f01985e748c023094e5e888e14b549db535
Gentes720 <lontsiangelin@gmail.com>
2024-12-12 19:26:08 +0100

    modified

--- Day3.4.py
+++ Day3.4.py
@@ -3,20 +3,20 @@ size = input("What size of pizza do you want? S, M, L")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0
-if size == "s" :
+if size == "S" :
     bill += 15 
-elif size == "l":
+elif size == "M":
     bill += 20
 else:
     bill += 25
 
-if add_pepperoni == "y":
-    if size == "s":
+if add_pepperoni == "Y":
+    if size == "S":
         bill += 2
     else:
         bill += 3
 
-if extra_cheese == "y":
+if extra_cheese == "Y":
     bill += 1
 
 print(f"your final bill is ${bill}")
